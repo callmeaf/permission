b2d26de898b722cbc5e5af2cfc07e52cbd1f2dbe
callmeaf <alikhoshkar2000@gmail.com>
2025-04-26 15:37:34 +0330

    add userCan for Perm facade

--- src/App/Services/PermissionService.php
+++ src/App/Services/PermissionService.php
@@ -2,6 +2,9 @@
 
 namespace Callmeaf\Permission\App\Services;
 
+use App\Models\User;
+use Callmeaf\Base\App\Repo\Contracts\CoreRepoInterface;
+use Callmeaf\Permission\App\Enums\PermissionCycle;
 use Callmeaf\Permission\App\Repo\Contracts\PermissionRepoInterface;
 
 class PermissionService
@@ -28,5 +31,28 @@ class PermissionService
         $permissionRepo = app(PermissionRepoInterface::class);
         $config = $permissionRepo->config;
         return $config['enums']['cycle']::cases();
+    }
+
+    /**
+     * @param string|PermissionCycle $permissionName
+     * @param ?string $repo
+     * @param ?User $user
+     * @return bool
+     */
+    public function userCan($permissionName,?string $repo = null,$user = null): bool
+    {
+        $user ??= request()->user();
+        if(is_string($permissionName)) {
+            return $user->can($permissionName);
+        }
+        /**
+         * @var CoreRepoInterface $repo
+         */
+        $repo = app($repo);
+        $packageName = \Base::getPackageNameFromModel(model: $repo->getModel()::class);
+        $packageName = str($packageName)->kebab()->toString();
+        $permissionName = $permissionName->value;
+
+        return $user->can("{$packageName}_{$permissionName}");
     }
 }
